1e8aedcf2916d1f99a2681c95ac03bb43229e0ec
msalehzadeh <sbc.mahdi@gmail.com>
2026-05-05 20:12:10 +0000

    Deploying to gh-pages from @ msalehzadeh/msalehzadeh.github.io@73af4892641499b58353f1963cc2da9fa32c1601 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "msalehzadeh/test.github.io",
+    "data-repo": "msalehzadeh/msalehzadeh.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
